a718a95782e62d006a628d8e5c5c70566b42f4b6
eransh1 <eranshbansal@gmail.com>
2023-01-19 13:50:50 +0530

    update sort bar in community

--- src/pages/Community/CommunityFinal.jsx
+++ src/pages/Community/CommunityFinal.jsx
@@ -35,6 +35,10 @@ import CommunityNews from "../../components/Community News/CommunityNews";
 import NewSkeleton from "../../components/Post Skeleton/News Skeleton/NewSkeleton";
 import { RxCrossCircled } from "react-icons/rx";
 import { FiEdit } from "react-icons/fi";
+import SortingNavbar from "./Sorting NavBar/SortingNavbar";
+import SortingNavbarTest from "./Sorting Navbar Test/SortingNavbarTest";
+import SortingNavbarOldest from "./Sorting Navbar Oldest/SortingNavbarOldest";
+import NoFollowingCard from "../../components/No Following Card/NoFollowingCard";
 const CommunityFinal = () => {
   const dispatch = useDispatch();
   const postData = [];
@@ -66,7 +70,7 @@ const CommunityFinal = () => {
   const [newScoll, setNewScroll] = useState(0);
   const [newsData, setNewsData] = useState();
   const [singleNews, setSingleNews] = useState(null);
-  console.log("newsData", newsData);
+  console.log("userDoc", userDoc);
 
   //FETCH LATEST NEWS
   const options = {
@@ -97,7 +101,7 @@ const CommunityFinal = () => {
     setScroll(window.scrollY);
   };
 
-  console.log("scroll", newScoll);
+ 
   // console.log("postsData", postsData);
 
   const updateWidth = () => {
@@ -691,228 +695,9 @@ const CommunityFinal = () => {
           >
             {/* FURTHER SORT POST SECTION */}
 
-            {/* <section id="sortPostSection">
-              <h2
-                onClick={() => setfurtherSortOptionClick((e) => !e)}
-                className="sortPostSectionHeading"
-              >
-                Showing:{" "}
-                <span className="sortPostSectionHeadingItemName">
-                  {sortOptionSelected.whose !== ""
-                    ? sortOptionSelected.whose
-                    : "None"}
-                </span>{" "}
-                <span style={{ display: "flex" }}>
-                  <img
-                    className={`downarrorwSortImg ${furtherSortOptionClick?'rotate-180':''}`}
-                    src="./images/down-filled-triangular-arrow.png"
-                    alt="downArrow"
-                  />
-                </span>{" "}
-              </h2>
-              {furtherSortOptionClick ? (
-                <div className="furtherSortPostSectionOptionContainer">
-                  <button
-                    onClick={() => {
-                      setSortOptionSelected((prev) => {
-                        return { ...prev, whose: "Everything" };
-                      });
-                      setfurtherSortOptionClick(false);
-                    }}
-                    className={
-                      sortOptionSelected.whose === "Everything"
-                        ? "furtherSortPostSectionOptionSelected"
-                        : "furtherSortPostSectionOption"
-                    }
-                  >
-                    Everything
-                  </button>
-                  <button
-                    onClick={() => {
-                      setSortOptionSelected((prev) => {
-                        return { ...prev, whose: "People You Follow" };
-                      });
-                      setfurtherSortOptionClick(false);
-                    }}
-                    className={
-                      sortOptionSelected.whose === "People You Follow"
-                        ? "furtherSortPostSectionOptionSelected"
-                        : "furtherSortPostSectionOption"
-                    }
-                  >
-                    People You Follow
-                  </button>
-                </div>
-              ) : null}
-            </section> */}
-            <section
-              onClick={() => {
-                setSortOptionSelected((prev) => {
-                  return { ...prev, whose: "Everything" };
-                });
-                setfurtherSortOptionClick(false);
-              }}
-              id="updatedSortPostSection"
-            >
-              Discover
-              <div
-                style={{
-                  display:
-                    sortOptionSelected.whose === "Everything" ? "" : "none",
-                }}
-                className="updatedSortPostSectionUnderLine"
-              ></div>
-            </section>
-
-            <section
-              onClick={() => {
-                setSortOptionSelected((prev) => {
-                  return { ...prev, whose: "People You Follow" };
-                });
-                setfurtherSortOptionClick(false);
-              }}
-              id="updatedSortPostSection"
-            >
-              Following
-              <div
-                style={{
-                  display:
-                    sortOptionSelected.whose === "People You Follow"
-                      ? ""
-                      : "none",
-                }}
-                className="updatedSortPostSectionUnderLine"
-              ></div>
-            </section>
-            {/* SORT POST SECTION */}
-            <section
-              onClick={() => setSortOptionClick((e) => !e)}
-              id="updatedSortPostSection"
-            >
-              {sortOptionSelected.time === "Popular Now"
-                ? "Popular"
-                : sortOptionSelected.time === "Newest"
-                ? "Latest"
-                : sortOptionSelected.time}
-              {sortOptionClick ? (
-                <section
-                  style={{
-                    right:
-                      sortOptionSelected.time === "Newest" ||
-                      sortOptionSelected.time === "Oldest"
-                        ? "-40px"
-                        : "-20px",
-                  }}
-                  className="dropDownSortOptionCont"
-                >
-                  <p
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      setSortOptionSelected((prev) => {
-                        return { ...prev, time: "Popular Now" };
-                      });
-                      setSortOptionClick(false);
-                    }}
-                    className="dropDownSortOptionContOptions"
-                  >
-                    Popular
-                  </p>
-                  <p
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      setSortOptionSelected((prev) => {
-                        return { ...prev, time: "Newest" };
-                      });
-                      setSortOptionClick(false);
-                    }}
-                    className="dropDownSortOptionContOptions"
-                  >
-                    Latest
-                  </p>
-                  <p
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      setSortOptionSelected((prev) => {
-                        return { ...prev, time: "Oldest" };
-                      });
-                      setSortOptionClick(false);
-                    }}
-                    className="dropDownSortOptionContOptions"
-                  >
-                    Oldest
-                  </p>
-                </section>
-              ) : null}
-            </section>
-            {/* <section id="sortPostSection">
-              <h2
-                onClick={() => setSortOptionClick((e) => !e)}
-                className="sortPostSectionHeading sortPostSectionHeadinggg"
-              >
-                Sorted By:{" "}
-                <span className="sortPostSectionHeadingItemName">
-                  {sortOptionSelected.time !== ""
-                    ? sortOptionSelected.time
-                    : "None"}
-                </span>{" "}
-                <span style={{ display: "flex" }}>
-                  <img
-                    className={`downarrorwSortImg ${sortOptionClick?'rotate-180':''}`}
-                    src="./images/down-filled-triangular-arrow.png"
-                    alt="downArrow"
-                  />
-                </span>
-              </h2>
-              {sortOptionClick ? (
-                <div className="sortPostSectionOptionContainer">
-                  <button
-                    onClick={() => {
-                      setSortOptionSelected((prev) => {
-                        return { ...prev, time: "Popular Now" };
-                      });
-                      setSortOptionClick(false);
-                    }}
-                    className={
-                      sortOptionSelected.time === "Popular Now"
-                        ? "sortPostSectionOptionSelected"
-                        : "sortPostSectionOption"
-                    }
-                  >
-                    <span>Popular Now</span>
-                  </button>
-                  <button
-                    onClick={() => {
-                      setSortOptionSelected((prev) => {
-                        return { ...prev, time: "Newest" };
-                      });
-                      setSortOptionClick(false);
-                    }}
-                    className={
-                      sortOptionSelected.time === "Newest"
-                        ? "sortPostSectionOptionSelected"
-                        : "sortPostSectionOption"
-                    }
-                  >
-                    Newest
-                  </button>
-                  <button
-                    onClick={() => {
-                      setSortOptionSelected((prev) => {
-                        return { ...prev, time: "Oldest" };
-                      });
-                      setSortOptionClick(false);
-                    }}
-                    className={
-                      sortOptionSelected.time === "Oldest"
-                        ? "sortPostSectionOptionSelected"
-                        : "sortPostSectionOption"
-                    }
-                  >
-                    Oldest
-                  </button>
-                </div>
-              ) : null}
-            </section> */}
+            {/* <SortingNavbarOldest setfurtherSortOptionClick={setfurtherSortOptionClick} sortOptionSelected={sortOptionSelected} furtherSortOptionClick={furtherSortOptionClick} setSortOptionSelected={setSortOptionSelected} setSortOptionClick={setSortOptionClick} sortOptionClick={sortOptionClick} /> */}
+            <SortingNavbarTest setSortOptionSelected={setSortOptionSelected} setfurtherSortOptionClick={setfurtherSortOptionClick} sortOptionSelected={sortOptionSelected} sortOptionClick={sortOptionClick} setSortOptionClick={setSortOptionClick}/>
+           
           </section>
 
           {/* POST SECTION */}
@@ -929,12 +714,12 @@ const CommunityFinal = () => {
               }
             >
               <section className="posts-containerr">
-                {displayPosts.length === 0 && (
+                {displayPosts.length === 0 &&sortOptionSelected.whose !== "People You Follow" && (
                   <div>
                     <PostSkeleton cards={2} />
                   </div>
                 )}
-
+                {userDoc?.network?.length===0&&sortOptionSelected.whose === "People You Follow"?<><NoFollowingCard setSortOptionSelected={setSortOptionSelected} setSortOptionClick={setSortOptionClick}/></>:null}
                 {displayPosts.map((item, index) => {
                   return (
                     <PostCard
