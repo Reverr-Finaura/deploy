61c013d9a2473ccd1fbd0f848320b1f88e1d94cd
Nikita <nikitakubavat33@gmail.com>
2023-03-06 20:40:44 +0530

    beta testing, startup ideas, competitor data added

---
+++ src/assets/New Courses Data/StartupIdea.js
@@ -0,0 +1,357 @@
+export const data = [
+  {
+    id: 1,
+    courseName: "THINKING OF A STARTUP IDEA",
+    image: "",
+    module: [
+      {
+        id: 1,
+        name: "HOW TO GENERATE STARTUP IDEAS",
+        title: "Module-1:",
+        image: "",
+        slides: [
+          {
+            type: "nonquiz",
+            title: "",
+            points: [
+              "Now right here's the way to generate a startup concept for what you are in all likelihood looking forward to. you may sit returned and think in element about your startup concept.",
+            ],
+            image: "",
+          },
+          {
+            type: "nonquiz",
+            title: "",
+            points: [
+              "We're going to talk approximately a way to do this in a moment. however in truth, this is not the first-class way to get ideas for startups. The best manner to get startup ideas is to organically word them.",
+            ],
+            image: "",
+          },
+          {
+            type: "nonquiz",
+            title: "",
+            points: [
+              "Terrific startup ideas are all around you, and you may see them everywhere in case you're geared up. The problem is that once humans sit down down and attempt to think of a startup idea, they tend to think handiest of bad thoughts. ",
+            ],
+            image: "",
+          },
+          {
+            type: "nonquiz",
+            title: "",
+            points: [
+              "A startup concept that emerges organically can in reality grow to be an awesome idea.  ",
+              "So first I need to speak approximately a way to organically generate thoughts for startups. in case you're no longer beginning a organisation right now, this should be your preferred technique",
+            ],
+            image: "",
+          },
+          {
+            type: "nonquiz",
+            title: "",
+            points: [
+              "Find exquisite startup thoughts and discover ways to convey them to you. To have a startup idea organically, you first need to realize what an excellent startup idea is so that you'll note it when it comes to you. then you definitely preserve an eye at the matters that appear to be lacking from the arena.",
+            ],
+            image: "",
+          },
+          {
+            type: "nonquiz",
+            title: "",
+            points: [
+              "if you are making plans to begin your adventure in the destiny, turning into an expert in something worthwhile could be very beneficial, and perhaps your high-quality bet is to get a job at the vanguard of a particular area. if you're at the leading edge of any area or area, you may see a good start before absolutely everyone else.",
+            ],
+            image: "",
+          },
+          {
+            type: "quiz",
+            title: "What type of idea is best for a startup?",
+            points: [
+              "A startup concept that emerges organically. ",
+              "A startup idea your friends suggest.",
+              "A startup idea that was successful in another country. ",
+              "A startup idea in high-growth sectors.",
+            ],
+            ans: "1",
+            image: "",
+          },
+          {
+            type: "quiz",
+            title: "You can organically generate startup ideas by……",
+            points: [
+              "Becoming an expert in a field and be at its vanguard to generate ideas before others ",
+              "Stealing ideas and making little changes.",
+              "hiring ex-startup founders.",
+              "having the best possible founding team.",
+            ],
+            ans: "1",
+            image: "",
+          },
+          {
+            type: "nonquiz",
+            title: "The seven ways",
+            points: [
+              "Good. Let's take a look at 7 ways for explicit idea generation for startups. We list all 7 ways exhaustively. This is almost every way we know how to generate ideas for startups. Now we know examples where all 7 worked and led to successful companies, but not all are equally good. Some are much more likely to generate good ideas.",
+            ],
+            image: "",
+          },
+          {
+            type: "nonquiz",
+            title: "",
+            points: [
+              "Start with what your team is particularly good at, and think of ideas that might have an undue advantage in implementation. What makes this so effective is that ideas conceived in this way automatically fit the founders/markets. It basically generates all ideas that are good for the founder/market.",
+            ],
+            image: "",
+          },
+          {
+            type: "nonquiz",
+            title: "",
+            points: [
+              "Second, Think of the things you want someone to do for you.",
+              "Third , Even if things don't go well for 10 years, what kind of work will make you happy? What are you really passionate about?",
+            ],
+            image: "",
+          },
+          {
+            type: "nonquiz",
+            title: "",
+            points: [
+              " Fourth, find a recent change in the world and think of possible ideas that are currently possible because of that change. For example, new technologies, new platforms for developers, new regulations, new challenges in the world. What waves can you ride?",
+              "Fifth, we seek new alternatives by finding recently successful companies.",
+            ],
+            image: "",
+          },
+          {
+            type: "nonquiz",
+            title: "",
+            points: [
+              "Sixth, you can also crowdsource ideas by talking to people you know and asking questions about problems you want to solve. It is especially interesting to work with people with specific specialties, for example those who work in interesting industry",
+              "Seventh Look for industries that appear to be broken. Any industry that appears to be broken is probably ripe for destruction.",
+            ],
+            image: "",
+          },
+          {
+            type: "quiz",
+            title:
+              "You can generate a startup idea by“Seeking new alternative by finding recently successful companies”",
+            points: ["True", "False"],
+            ans: "1",
+            image: "",
+          },
+        ],
+      },
+      {
+        id: 2,
+        name: "HOW TO EVALUATE A STARTUP IDEA",
+        title: "Module-2:",
+        image: "",
+        slides: [
+          {
+            type: "nonquiz",
+            title: "",
+            points: [
+              "The first criteria is how big is this idea? The best way to judge this, we think, is to look for existing large companies that do something similar. So, suppose you want to start a new kind of bank. Well, there already are a bunch of banks, and they're big companies, so that suggests that this idea could be big.",
+            ],
+            image: "",
+          },
+          {
+            type: "nonquiz",
+            title: "",
+            points: [
+              "Your idea could also score well here if you're doing something in a market that's small today, but has a real chance of being huge in the future.",
+            ],
+            image: "",
+          },
+          {
+            type: "nonquiz",
+            title: "",
+            points: [
+              "The next criteria is how sure are you that you're solving a big problem? Ideally, you should  have personal experience with this problem. And finally, do you have a new, important insight into this idea? And a great example of this is Airbnb.",
+            ],
+            image: "",
+          },
+          {
+            type: "quiz",
+            title: "Best way to evaluate your startup idea is…",
+            points: [
+              "Ask friend and family",
+              "to look for existing large companies that do something similar.",
+              "Consult a financial advisor",
+            ],
+            ans: "2",
+            image: "",
+          },
+          {
+            type: "nonquiz",
+            title: " ",
+            points: [
+              "When Airbnb got started, most people thought that the idea of allowing strangers you met on the internet to sleep in your apartment was really weird, and probably dangerous. But the Airbnb founders had actually tried this. They'd invite strangers from the internet to stay with them, and they realized it was really fun. So they had this insight that everyone else was wrong about this, and that's what made Airbnb such a good idea.",
+            ],
+            image: "",
+          },
+          {
+            type: "nonquiz",
+            title: "",
+            points: [
+              "Here are some other signs to look for that your idea is probably a good one. You're making something you personally want to have. It turns out it's much easier to start a successful company if you're making something you yourself want, instead of something other people want.",
+            ],
+            image: "",
+          },
+          {
+            type: "quiz",
+            title:
+              "Ideally, you should  have _______ the  problem you are solving.",
+            points: [
+              "Personal experience with",
+              "passion about",
+              "a degree related to",
+              "All of these",
+            ],
+            ans: "1",
+            image: "",
+          },
+          {
+            type: "nonquiz",
+            title: "",
+            points: [
+              "For one, you start with at least one user, yourself, but more importantly, when you're building for yourself, you can trust your intuition about what to build. If you're building for other people, you have to guess what they would want.",
+            ],
+            image: "",
+          },
+          {
+            type: "nonquiz",
+            title: "",
+            points: [
+              "Another sign is that this only recently became possible. If something recently changed in the world, like a new technology or a new product you can build on top of, that's often a sign that a new opportunity has been created. Or, there are successful companies that do something similar.",
+            ],
+            image: "",
+          },
+          {
+            type: "nonquiz",
+            title: "",
+            points: [
+              "Last topic about evaluating startup ideas is to talk about filters. Filters are bad reasons to reject startup ideas.",
+            ],
+            image: "",
+          },
+          {
+            type: "nonquiz",
+            title: "",
+            points: [
+              "The most dangerous part of filters is that they will cause you to reject your best startup ideas unconsciously, before they even bubble up to your conscious mind, and there are four big ones that are extremely important to understand.",
+            ],
+            image: "",
+          },
+          {
+            type: "nonquiz",
+            title: "",
+            points: [
+              "Almost everyone has these filters. And one of the most impactful things you can do to become good at generating startup ideas is to learn what these are, and to turn them off.",
+            ],
+            image: "",
+          },
+          {
+            type: "quiz",
+            title: "Most dangerous part of filters is…….",
+            points: [
+              "that they will cause you to reject your best startup ideas unconsciously ",
+              "that they will cause you to accept your best startup ideas unconsciously",
+              "that they will cause you to select your best startup ideas unconsciously",
+            ],
+            ans: "1",
+            image: "",
+          },
+        ],
+      },
+      {
+        id: 3,
+        name: "MISTAKES FOUNDERS DO",
+        title: "",
+        image: "",
+        slides: [
+          {
+            type: "nonquiz",
+            title: "",
+            points: [
+              "The first and most common mistake is the belief that good ideas come first. It usually looks like this: People believe that the key to a successful startup is starting with a brilliant idea. So they wait for a brilliant idea before starting anything.",
+            ],
+            image: "",
+          },
+          {
+            type: "nonquiz",
+            title: "",
+            points: [
+              "When you look at successful modern companies like Google or Facebook, it's easy to think, \"Wow, what a brilliant idea.\" But we can say that it only looks so in retrospect. When Google was launched, it was the 20th search engine. And when Facebook was launched, it was the 20th social network. What made them successful wasn't a great initial idea. It was a reasonably good initial idea combined with excellent execution.",
+            ],
+            image: "",
+          },
+          {
+            type: "nonquiz",
+            title: "",
+            points: [
+              "The following real number is the exact opposite. It's about taking the first idea that comes to mind without stopping and critically thinking about whether it's good or not. And it's amazing how common it is. That's the problem. If your startup succeeds, you will spend a lifetime working on it. And if you're going to spend a few years of your life doing something, wouldn't it make sense to spend a few weeks deciding what to do first?",
+            ],
+            image: "",
+          },
+          {
+            type: "nonquiz",
+            title: "",
+            points: [
+              'Surprisingly, very few founders do this, and when they do they are actually ahead of the curve. My argument is that if you imagine a spectrum where one end is "try the idea that comes to mind first" and the other end "wait for the perfect idea", you want to be somewhere in between. You should think of your original idea as a good starting point.',
+            ],
+            image: "",
+          },
+          {
+            type: "nonquiz",
+            title: "",
+            points: [
+              "Startup ideas change over time. Take Airbnb as an example. Initially, Airbnb was literally a site for renting airbeds in someone else's home. Over time, it evolved into all types of vacation rentals. So, you need to choose a good starting point. If you have a good starting point, even if your initial idea isn't very good, you need to improve it to get a good one. On the other hand, if you choose a bad starting point, you can't turn it into a good starting point. You have to start all over again.",
+            ],
+            image: "",
+          },
+          {
+            type: "nonquiz",
+            title: "",
+            points: [
+              "The third mistake is to start with the solution, not the problem. And we'll give you an example. Imagine coming up with a startup called Uber for plumbers. This app brings up the plumber at the push of a button. This is the solution. What problem do you solve? we do not know. It can be difficult to find a plumber. This may be a real problem, but when an idea like this comes up, I believe it starts with a solution. so there is a term.",
+            ],
+            image: "",
+          },
+          {
+            type: "nonquiz",
+            title: "",
+            points: [
+              "This is called SISP (Solving in Search of Problem). And that's usually not good because it usually doesn't find the problem. It's much better to start with the problem and then find a solution.",
+            ],
+            image: "",
+          },
+          {
+            type: "nonquiz",
+            title: "",
+            points: [
+              "The final mistake is believing that startup ideas are hard to come by. In fact, it is easy to find because there are so many real problems left in the world. If you're having trouble finding it, you haven't learned how to do it yet. Learn to discover great startup ideas and you'll see them everywhere.",
+            ],
+            image: "",
+          },
+          {
+            type: "quiz",
+            title: "which is a better method to look for startup ideas ?",
+            points: [
+              "start with the problem and then find a solution",
+              "start with the solution and then evaluate the problem it is solving. ",
+            ],
+            ans: "1",
+            image: "",
+          },
+          {
+            type: "quiz",
+            title: "what is SISP?",
+            points: [
+              "Searching in Solved Problems",
+              "Solving in Search of Problem ",
+              "Solving initially solvable Problems. ",
+            ],
+            ans: "2",
+            image: "",
+          },
+        ],
+      },
+    ],
+  },
+];
