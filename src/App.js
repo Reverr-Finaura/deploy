0228dc24d71ee36fdd3ff3e44a76207fd8b6d81f
eransh1 <eranshbansal@gmail.com>
2023-04-15 22:58:15 +0530

    remove dummy data

--- src/App.js
+++ src/App.js
@@ -94,7 +94,6 @@ import GoogleSignupInfoPage from "./pages/Onboarding Screens/Google Signup Info 
 import RsFiveOneZero from "./pages/RsFiveOneZero/RsFiveOneZero";
 import Upgrade from "./pages/Upgrade/Upgrade";
 import Chat from "./pages/Chat/Chat"
-import DummyData from "./components/Dummy Data/DummyData";
 // import CompetitorAnalysis from "./pages/New Courses/Courses/CompetitorAnalysis";
 // import BuildAudience from "./pages/New Courses/Courses/BuildAudience";
 
@@ -307,7 +306,6 @@ function App() {
         ></Route> */}
         <Route path="/upgrade" element={<Upgrade />} />
         <Route path="/messages" element={<Chat/>}></Route>
-        <Route path="/dummyData" element={<DummyData/>}></Route>
       </Routes>
     </>
   );
