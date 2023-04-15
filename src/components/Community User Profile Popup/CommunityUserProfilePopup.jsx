2d00276f6e1ae697ef2c731522cd3188bf239422
eransh1 <eranshbansal@gmail.com>
2023-04-15 15:11:43 +0530

    fixed community friend request

--- src/components/Community User Profile Popup/CommunityUserProfilePopup.jsx
+++ src/components/Community User Profile Popup/CommunityUserProfilePopup.jsx
@@ -22,7 +22,7 @@ const CommunityUserProfilePopup = ({setPostsAuthorIsClick,postsAuthorInfo,setPos
     const[choiceButtonClick,setChoiceButtonClick]=useState("Info")
     const[selectedUserPostsArray,setSelectedUserPostsArray]=useState(null)
 
-  console.log(postsAuthorInfo,"postsAuthorInfo")  
+  console.log("postsAuthorInfo",postsAuthorInfo)  
 
 const fetchSelectedUserPosts=(postArray)=>{
     let SelectedUserPostData=[]
@@ -55,7 +55,7 @@ const updateUserSendRequestArray=async()=>{
 //HANDLE FOLLOW REQUEST CLICK
 
 const handleFollowUserClick=async()=>{
-const userRequestArray=postsAuthorInfo.receivedRequests.concat([user?.user?.email])
+const userRequestArray=postsAuthorInfo.receivedRequests?postsAuthorInfo.receivedRequests:postsAuthorInfo.recivedRequests.concat([user?.user?.email])
 
 const userDocumentRef=doc(db,"Users",postsAuthorInfo.email)
 
